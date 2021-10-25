5ada6fbedbf09df2a2a5455472429d71e8fef53e
Ayush Gupta <71512499+ayush7138@users.noreply.github.com>
2021-10-25 23:48:38 +0300

    Update run_fse.sh

--- run_fse.sh
+++ run_fse.sh
@@ -5,9 +5,16 @@ set -x
 virtualenv -p python3 .
 source ./bin/activate
 
+pip install tensorflow
+make requirements
+
 wget https://storage.googleapis.com/local-implicit-grids/pretrained_ckpt.zip
 unzip  pretrained_ckpt.zip && rm pretrained_ckpt.zip
 
 mkdir -p demo_data
 wget https://cs.uwaterloo.ca/~c2batty/bunny_watertight.obj
 mv -f bunny_watertight.obj demo_data
+
+make run
+
+make test
