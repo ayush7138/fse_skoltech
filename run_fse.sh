f62ba69b1b55341d11011cf429fa174d9864db9d
Ayush Gupta <71512499+ayush7138@users.noreply.github.com>
2021-10-26 04:59:54 +0300

    Update run_fse.sh

--- run_fse.sh
+++ run_fse.sh
@@ -5,6 +5,8 @@ set -x
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+export PYTHONPATH="$PWD:$PYTHONPATH"
+
 pushd $SCRIPTPATH
 
 apt install python3-pip
@@ -15,8 +17,8 @@ make requirements
 wget https://storage.googleapis.com/local-implicit-grids/pretrained_ckpt.zip
 unzip  pretrained_ckpt.zip && rm pretrained_ckpt.zip
 
-wget http://storage.googleapis.com/local-implicit-grids/demo_data.zip
-unzip demo_data.zip; rm demo_data.zip
+# wget http://storage.googleapis.com/local-implicit-grids/demo_data.zip
+# unzip demo_data.zip; rm demo_data.zip
 
 mkdir -p demo_data
 wget https://cs.uwaterloo.ca/~c2batty/bunny_watertight.obj
