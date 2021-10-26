7a5337f211bbe80852f8b91b8eaade427ef99cc4
Ayush Gupta <71512499+ayush7138@users.noreply.github.com>
2021-10-26 17:54:19 +0300

    Update test_version.py

--- test_version.py
+++ test_version.py
@@ -1,26 +1,23 @@
 #! /usr/bin/env python3
 
-import abs1
 import numpy as np
-import plyfile
 import scipy
-import sci-image as sci
 import trimesh
 import six
+import skimage
 import tensorflow as tf
 
 import unittest
 
 class ComputeVersionTest(unittest.TestCase):
   def test_v(self):
-    self.assert(abs1.__version__ >= '0.7.1')
-    self.assert(np.__version__ == '1.19.2')
-    self.assert(plyfile.__version__ >= '0.7.1')
-    self.assert(scipy.__version__ >= '1.7.1')
-    self.assert(sci.__version__ >= '0.15.0')
-    self.assert(trimesh.__version__ >= '3.2.12')
-    self.assert(six.__version__ == '1.15.0')
-    self.assert(tf.__version__ >= '1.14.0')
-    
+    self.assertEqual(np.__version__, '1.19.2')
+    self.assertEqual(skimage.__version__, '0.18.3')
+    self.assertEqual(scipy.__version__, '1.7.1')
+    self.assertEqual(trimesh.__version__, '3.9.34')
+    self.assertEqual(six.__version__, '1.15.0')
+    self.assertEqual(tf.__version__, '2.6.0')
+
 if __name__ == '__main__':
   unittest.main()
+
