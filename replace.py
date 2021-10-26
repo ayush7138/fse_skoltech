e402b57e27c883fa4e2b774938c5747472fa7b46
Ayush Gupta <71512499+ayush7138@users.noreply.github.com>
2021-10-26 03:48:16 +0300

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import fileinput
 
-filename = ['resample_geometry.py', 'reconstruct_geometry.py']
+filename = ['resample_geometry.py', 'reconstruct_geometry.py', 'core/evaluator.py', 'implicit_nets.py', 'local_implicit_grid_layer.py', 'model_g2g.py', 'model_g2v.py', 'point_utils.py', 'postprocess.py', 'reconstruction.py', 'regular_grid_interpolation.py']
 
-for i in len(filename):
-  with fileinput.FileInput(filename[i], inplace=True, backup='.bak') as file:
-      for line in file:
-          print(line.replace('tensorflow_graphics.projects.local_implicit_grid.core', 'core'), end='')
+for i in filename:
+    # Read in the file
+    file_py = open(i, 'r')
+    filedata = file_py.read()
+
+    # Replace the target string
+    filedata = filedata.replace('tensorflow_graphics.projects.local_implicit_grid.core', 'core')
+
+    # Write the file out again
+    file_py = open(i, 'w')
+    file_py.write(filedata)
